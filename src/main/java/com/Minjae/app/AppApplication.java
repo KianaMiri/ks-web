2c934bf7cabf05af177f6cddaff511f6b4438250
Kiana <kianapark85@gmail.com>
2020-05-03 22:16:20 +0900

    Controller data connected

--- src/main/java/com/Minjae/app/AppApplication.java
+++ src/main/java/com/Minjae/app/AppApplication.java
@@ -1,13 +1,26 @@
 package com.Minjae.app;
 
+import entity.Person;
+import entity.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication implements CommandLineRunner {
+
+	private final PersonRepository personRepository;
+
+	public AppApplication(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		personRepository.save(new Person("???", "??? ??", "hello@test.com", "010-1234-5678"));
+	}
 }
--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,14 +1,24 @@
 package controller;
 
+import entity.Person;
+import entity.repository.PersonRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-
 public class IndexController {
 
+    private final PersonRepository personRepository;
+
+    public IndexController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        Person person = personRepository.findAll().stream().findFirst().orElse(null);
+        model.addAttribute("person", person);
         return "resume";
     }
 }
