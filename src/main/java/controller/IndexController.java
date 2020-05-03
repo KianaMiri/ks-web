e6f8b5152ade495f59c1032eaebf8882ed4e11a0
Kiana <kianapark85@gmail.com>
2020-05-03 19:01:37 +0900

    Added README.md 파일 추가

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.sangkon.app;
+package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "resume";
     }
 }
